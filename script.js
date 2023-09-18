7e661546bb2807110415d5ead8d5668526330939
David Johrden <david.johrden@gmail.com>
2023-09-18 21:31:17 +0200

    update calculator

--- script.js
+++ script.js
@@ -5,29 +5,20 @@ const contentHistory = document.querySelector("#history");
 let valCurrentDisplay = 0;
 let valLastDisplay = 0;
 let valHistory = 0;
+let lastOperator = null;
 
 function appendValue(number) {
   valCurrentDisplay = valCurrentDisplay * 10 + number;
   changeCurrentDisplay();
 }
 
-function appendAddition(operator) {
+function appendOperator(operator) {
   checkOperator(operator);
-  operator = "+";
+
   valCurrentDisplay = 0;
   changeCurrentDisplay();
   changeLastDisplay(operator);
 }
-function appendSubtraction(operator) {
-  checkOperator();
-  valCurrentDisplay = 0;
-  operator = "-";
-  changeCurrentDisplay();
-  changeLastDisplay(operator);
-}
-function appendMultiplication(operator) {}
-function appendDivision(operator) {}
-function calculate() {}
 
 function clearDisplay() {
   valCurrentDisplay = 0;
@@ -43,22 +34,34 @@ function changeCurrentDisplay() {
 function changeLastDisplay(operator) {
   contentLastDisplay.innerHTML = valLastDisplay + operator;
 }
+function calculate() {
+  checkOperator(lastOperator);
+  valCurrentDisplay = valLastDisplay;
+  valLastDisplay = " ";
+  changeLastDisplay(" ");
+
+  changeCurrentDisplay();
+}
 
 function checkOperator(operator) {
   let interVal = 0;
-  if (operator === "+") {
+  if (lastOperator === null) {
+    interVal = valCurrentDisplay;
+  }
+  if (lastOperator === "+") {
     interVal = valLastDisplay + valCurrentDisplay;
   }
-  if (operator === "-") {
+  if (lastOperator === "-") {
     interVal = valLastDisplay - valCurrentDisplay;
   }
-  if (operator === "*") {
+  if (lastOperator === "*") {
     interVal = valLastDisplay * valCurrentDisplay;
   }
-  if (operator === "/") {
+  if (lastOperator === "/") {
     interVal = valLastDisplay / valCurrentDisplay;
   }
   valLastDisplay = interVal;
+  lastOperator = operator;
 }
 // function calculate() {
 //   const test1 = contentCurrentDisplay.innerHTML;
@@ -69,3 +72,21 @@ function checkOperator(operator) {
 
 //   contentMemory.innerHTML += "<p>" + test1 + "</p>";
 // }
+
+/**
+ * 1.   Eingabe Zahl (1)
+ * 2.   addition
+ * 3.   Eingabe nächste Zahl (5)
+ * 4.   substraction
+ * 5.   1+5 wird gerechnet und als result angezeigt (6)
+ * 6.   Eingabe nächste Zahl (3)
+ * 7.   multiplication
+ * 8.   6-3 wird gerechnet und als result angezeigt (3)
+ * 9.   Eingabe nächste Zahl (3)
+ * 10.  xxxx
+ * 11.  3*3 wird gerechnet und als result angezeigt (9)
+ */
+
+/**
+ * Wenn lastDisplay === 0 dann keine operation ausführen
+ */
